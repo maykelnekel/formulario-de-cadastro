6470ecebd7ae2deea1858fb096bfe7831e51a4ea
Maykel Nekel <maykelnekel@gmail.com>
2021-07-20 23:42:48 -0300

    Bug no Router Dom, nao aparece o conteúdo das paginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Form from './Pages/Form';
+import LoginPage from './Pages/LoginPage';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path='/register'>
+          <Form/>
+        </Route>
+        <Route exact path='/login-page>'>
+          <LoginPage/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/Form/index.jsx
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
+export default function Form () {
+    return(
+        <p>
+            Form
+        </p>
+    )
+}
